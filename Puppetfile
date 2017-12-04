220b47c72e00c71a7398031185190d63f56c7dba
Zdenko Vrabel <sn3d@Zdenkos-MacBook-Pro.local>
2017-12-04 23:46:41 +0100

    networking

--- Puppetfile
+++ Puppetfile
@@ -5,6 +5,7 @@ mod 'puppetlabs/concat'
 mod 'puppetlabs/firewall'
 mod 'puppetlabs/java', '2.3.0'
 
+mod 'camptocamp-systemd', '1.1.1'
 mod 'puppet-archive', '2.2.0'
 mod 'example42-network', '3.3.9'
 mod 'theforeman-dhcp', '4.0.2'
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,14 @@
 node 'puppet.eastcode.sys' {
 
-  network::interface { 'eth0':
-    ipaddress => '192.168.2.4',
+  network::interface { 'enp0s3':
+    ipaddress => '192.168.2.1',
     netmask   => '255.255.0.0',
     gateway   => '192.168.0.1'
   }
 
   include dns::server
 
-  dns::server::options { '/etc/bind/named.conf.options':
+  dns::server::options { '/etc/named/named.conf.options':
     forwarders => [ '8.8.8.8', '8.8.4.4' ]
   }
 
@@ -24,41 +24,32 @@ node 'puppet.eastcode.sys' {
     nameservers => ['ns1']
   }
 
-  dns::record::a { 'puppet':
-    zone => 'eastcode.sys',
-    data => ['192.168.2.1']
+  dns::record::a {
+    'puppet': zone => 'eastcode.sys', data => ['192.168.2.1'];
+    'agent1': zone => 'eastcode.sys', data => ['192.168.2.2'];
+    'agent2': zone => 'eastcode.sys', data => ['192.168.2.3'];
+    'pi':     zone => 'eastcode.sys', data => ['192.168.2.4'];
   }
 
-  dns::record::a { 'agent1':
-    zone => 'eastcode.sys',
-    data => ['192.168.2.2']
-  }
-
-  dns::record::a { 'agent2':
-    zone => 'eastcode.sys',
-    data => ['192.168.2.3']
-  }
-
-  dns::record::a { 'pi':
-    zone => 'eastcode.sys',
-    data => ['192.168.2.4']
-  }
-
-  dns::record::cname { 'www':
-    zone => 'eastcode.sys',
-    data => 'agent1.eastcode.sys'
+  dns::record::cname {
+    'www': zone => 'eastcode.sys', data => 'agent1.eastcode.sys';
   }
 
   class { 'dhcp':
-    interfaces   => ['eth0']
+    interfaces   => ['enp0s3']
   }
 
   dhcp::pool{ 'dc1.eastcode.sys':
     network     => '192.168.2.0',
-    mask        => '255.255.0.0',
+    mask        => '255.255.255.0',
     range       => '192.168.2.10 192.168.2.200',
     gateway     => '192.168.2.1',
     nameservers => ['192.168.2.1', '192.168.0.1']
+  }
+
+  dhcp::host {
+    'server1': mac => "08:00:27:6B:02:02", ip => "192.168.2.2";
+    'server2': mac => "08:00:27:6B:02:03", ip => "192.168.2.3";
   }
 
 }
